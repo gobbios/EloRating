792b1c2af0a8c82a0ded09444734b8cd6c4a6abe
gobbios <christofneumann1@gmail.com>
2017-01-23 10:27:16 +0100

    added e.single and winprob

---
+++ R/e.single.R
@@ -0,0 +1,88 @@
+# e.single 15_09_29
+# used in other functions:
+# elo.seq()
+# randomelo()
+
+
+#' Elo ratings for a single interaction
+#'
+#' calculate/update Elo ratings for a single dyadic interaction
+#'
+#' @param ELO1old,ELO2old Elo rating of the first and second individual
+#' @param outcome "1" = first individual wins and second looses\cr
+#' "2" = second individual wins and first looses\cr
+#' "0" = interaction ends in a draw/tie (no winner and no looser)
+#' @param k \emph{k} factor
+#' @param normprob logical (by default \code{TRUE}). Should a normal curve be assumed for calculating the winning/losing probablities, or a logistic curve. See \code{\link{winprob}} for details
+#'
+#' @return integer vector of length 2 with updated ratings of first and second individual after the interaction
+#'
+#' @references Albers, P. C. H. & de Vries, H. 2001. Elo-rating as a tool in the sequential estimation of dominance strengths. Animal Behaviour, 61, 489-495. (\href{https://dx.doi.org/10.1006/anbe.2000.1571}{DOI: 10.1006/anbe.2000.1571})
+#'
+#' @importFrom stats pnorm
+#'
+#' @author Christof Neumann
+#'
+#' @examples
+#' e.single(ELO1old=1200, ELO2old=1000, outcome=1, k=100)
+#' # same as before
+#' e.single(ELO1old=1000, ELO2old=1200, outcome=2, k=100)
+#' # an undecided interaction
+#' e.single(ELO1old=1200, ELO2old=1000, outcome=0, k=100)
+#' # if rating differences are too big, no change occurs
+#' # if higher-rated individual wins
+#' e.single(ELO1old=2000, ELO2old=1000, outcome=1, k=100)
+#' # same as before but lower-rated individual wins and
+#' # therefore wins maximum number of points possible (i.e. k)
+#' e.single(ELO1old=2000, ELO2old=1000, outcome=2, k=100)
+#'
+#' @export
+
+
+
+e.single <- function(ELO1old, ELO2old, outcome, k=100, normprob=TRUE) {
+
+  # outcome must be one of the following:
+  # "1" = first individual wins and second looses
+  # "2" = second individual wins and first looses
+  # "0" = interaction ends in a draw/tie (no winner and no looser)
+
+  # normprob:
+  # should winning probability calculated from normal curve or logistic curve?
+
+  # make sure ELO ratings are given as numerics
+  # omit because inside functions this is true...
+  # ELO1old <- as.numeric(as.character(ELO1old))
+  # ELO2old <- as.numeric(as.character(ELO2old))
+
+  # calculates the difference between the two ratings
+  ELO_diff <- (ELO1old - ELO2old)
+
+  if(normprob) {
+    # z score based on fixed SD=200 (see Elo 1978)
+    z_score <- ELO_diff/(200*sqrt(2))
+    # calculates the winning probabilty
+    p_win <- pnorm(z_score)
+  } else {
+    # winning probability based on logistic approach
+    p_win <- 1 - 1 / (1 + 10^(ELO_diff/400))
+  }
+
+
+  # product of winning probabilty and k-factor
+  kp <- k * p_win
+
+  # the actual updating calculations
+  if(outcome==1) { ELO1new <- ELO1old - kp + k
+  ELO2new <- ELO2old + kp - k
+  }
+  if(outcome==2) { ELO1new <- ELO1old - kp
+  ELO2new <- ELO2old + kp
+  }
+  if(outcome==0) { ELO1new <- ELO1old - kp + 0.5 * k
+  ELO2new <- ELO2old + kp - 0.5 * k
+  }
+  # returning the updated ratings
+  # ratings are rounded to integers
+  return(round(c(ELO1new, ELO2new), 0))
+}
---
+++ R/winprob.r
@@ -0,0 +1,45 @@
+# winprob 15_09_29
+# used in other functions:
+# ---
+
+
+#' expected probability of winning
+#'
+#' calculate expected probability of winning given known strength of both opponents
+#'
+#' @param elo1 Elo rating from individual for which the winning probability should be calculated
+#' @param elo2 Elo rating of the opponent
+#' @param normprob logical (by default \code{TRUE}). Should a normal curve be assumed for calculating the winning/losing probablities, or a logistic curve. See \code{\link{winprob}} for details
+#'
+#' @details Elo (1978) proposed three ways of calculating winning probabilities (section 8.73), one of which (the `linear' approach) is ignored here because it ``lacks the sophistication and flexibility to express the limitation on D [rating difference] and the deflation controls required for integrity of the ratings''. Between the two remaining approaches (normal and logistic), Elo favored initially the normal over the logistic function, though he writes that the logistic function ``better reflects large deviations in an extended series''. Because of Elo's initial preference, the default approach taken by the package's functions is the normal one, though it can be changed to the logistic one if desired.
+#'
+#' @return numeric, expected chance of first individual to win an interacation with the second individual
+#'
+#' @references Elo, A. E. 1978. The Rating of Chess Players, Past and Present. New York: Arco.
+#'
+#' @author Christof Neumann
+#'
+#' @importFrom stats pnorm
+#'
+#' @examples
+#' winprob(1200,1000)
+#' winprob(1000,1200)
+#' winprob(1000,1000)
+#'
+#' @export
+
+
+
+
+winprob <- function(elo1, elo2, normprob=TRUE) {
+
+  if(normprob) {
+    # z score based on fixed SD=200 (see Elo 1978)
+    z_score <- (elo1 - elo2)/(200*sqrt(2))
+    p_win <- pnorm(z_score)
+  } else {
+    p_win <- 1 - 1 / (1 + 10^((elo1 - elo2)/400))
+  }
+
+  return(p_win)
+}
