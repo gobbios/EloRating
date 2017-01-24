65b643c43874753d131c8fbf17e5fb3aae596377
gobbios <christofneumann1@gmail.com>
2017-01-24 10:37:01 +0100

    added print methods

---
+++ R/print.elo.R
@@ -0,0 +1,22 @@
+
+
+#' prints its argument
+#'
+#' prints its argument
+#'
+#' @param x result from \code{\link{elo.seq}}
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
+#'
+#' @author Christof Neumann
+#'
+#' @examples
+#' data(adv)
+#' SEQ <- elo.seq(winner=adv$winner, loser=adv$loser, Date=adv$Date)
+#' print(SEQ)
+
+print.elo <- function(x, ...){
+
+  cat("\nElo ratings from", x$misc["nID"], "individuals\n")
+}
---
+++ R/print.seqchecknopres.r
@@ -0,0 +1,26 @@
+#' prints its argument
+#'
+#' prints its argument
+#'
+#' @param x result from \code{\link{seqcheck}}
+#' @param ... further arguments passed to or from other methods
+#'
+#' @export
+#'
+#' @author Christof Neumann
+#'
+#' @examples
+#' data(adv); data(advpres)
+#' SEQC <- seqcheck(winner=adv$winner, loser=adv$loser, Date=adv$Date)
+#' print(SEQC)
+
+print.seqchecknopres <- function(x, ...) {
+  if(x$checksum["presence"]==1) cat("no presence data supplied\n")
+
+  if(x$checksum["IDcheck"]==1) cat("IDs occur in the data with inconsistent capitalization (ignore if on purpose)...WARNING\n")
+  if(x$checksum["selfinteractions"]==1) cat(x$selfinteractions, "\n", sep="")
+  if(x$checksum["length"]==1) cat("your data vectors do not match in length...ERROR\n")
+  if(x$checksum["singledayobs"]==1) cat("the following individuals were observed only on one day: ", paste(x$singledaycases, collapse=", "), " ...WARNING\n", sep="" )
+
+  if(x$checksum["selfinteractions"]+x$checksum["IDcheck"]+x$checksum["length"]+x$checksum["singledayobs"]==0) cat("Everything seems to be fine with the interaction sequence...OK\n")
+}
